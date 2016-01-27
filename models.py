8136c7acd0cc8a223b1cb4200b30b726f4828382
Jernej Kos <jernej@kos.mx>
2016-01-27 17:02:46 +0100

    Register the Data model with query observers.

--- models.py
+++ models.py
@@ -9,6 +9,7 @@ from resolwe.flow import models as flow_models, serializers as flow_serializers,
 # Register all the models with the query observer pool.
 # TODO: This should be moved to a separate application.
 pool.register_model(flow_models.Project, flow_serializers.ProjectSerializer, flow_views.ProjectViewSet)
+pool.register_model(flow_models.Data, flow_serializers.DataSerializer, flow_views.DataViewSet)
 
 # Setup model notifications.
 observer_client = client.QueryObserverClient()
