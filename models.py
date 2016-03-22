f54faa3393c67d860ed3a5015505b8977e4d7bf7
hadalin <primoz@genialis.com>
2016-03-22 11:24:19 +0100

    Register DescriptorSchemaViewSet

--- models.py
+++ models.py
@@ -11,6 +11,7 @@ from resolwe.flow import models as flow_models, serializers as flow_serializers,
 pool.register_viewset(flow_views.CollectionViewSet)
 pool.register_viewset(flow_views.DataViewSet)
 pool.register_viewset(flow_views.ProcessViewSet)
+pool.register_viewset(flow_views.DescriptorSchemaViewSet)
 
 # Setup model notifications.
 observer_client = client.QueryObserverClient()
