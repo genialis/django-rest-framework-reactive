4cf9b69d136839dbaf9ca461073ec0ce28f91893
Jernej Kos <jernej@kos.mx>
2016-07-09 23:49:30 +0200

    Don't crash if _subscriber is not set.

--- rest_framework_reactive/websockets.py
+++ rest_framework_reactive/websockets.py
@@ -1,7 +1,7 @@
 import cPickle as pickle
 
 from django.conf import settings
-from ws4redis import redis_store, subscriber
+from ws4redis import subscriber
 
 from . import connection
 
@@ -13,10 +13,11 @@ class QueryObserverSubscriber(subscriber.RedisSubscriber):
         super(QueryObserverSubscriber, self).set_pubsub_channels(request, channels)
 
     def release(self):
-        # Publish a message that the client has disconnected.
-        self._connection.publish(connection.QUERYOBSERVER_REDIS_CHANNEL, pickle.dumps({
-            'event': 'subscriber_gone',
-            'subscriber': self._subscriber,
-        }))
+        if hasattr(self, '_subscriber'):
+            # Publish a message that the client has disconnected.
+            self._connection.publish(connection.QUERYOBSERVER_REDIS_CHANNEL, pickle.dumps({
+                'event': 'subscriber_gone',
+                'subscriber': self._subscriber,
+            }))
 
         super(QueryObserverSubscriber, self).release()
