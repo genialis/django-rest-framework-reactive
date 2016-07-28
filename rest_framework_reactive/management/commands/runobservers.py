1b253ebcd91fb4412f474a553e7a91b0e72cf672
hadalin <hadalin.primoz@gmail.com>
2016-07-28 16:32:22 +0200

    Add autoreload feature

--- rest_framework_reactive/management/commands/runobservers.py
+++ rest_framework_reactive/management/commands/runobservers.py
@@ -4,19 +4,21 @@ import gevent.select
 from gevent import pywsgi, event
 import psycogreen.gevent
 
-# Patch the I/O primitives and psycopg2 database driver to be greenlet-enabled.
-gevent.monkey.patch_all()
-psycogreen.gevent.patch_psycopg()
-
 import redis.connection
-redis.connection.select = gevent.select.select
 
 from django import db
 from django.core import exceptions
 from django.core.management import base
+from django.utils import autoreload
 
 from ... import rpc, connection
 from ...pool import pool
+
+# Patch the I/O primitives and psycopg2 database driver to be greenlet-enabled.
+gevent.monkey.patch_all()
+psycogreen.gevent.patch_psycopg()
+
+redis.connection.select = gevent.select.select
 
 
 class Command(base.BaseCommand):
@@ -26,7 +28,22 @@ class Command(base.BaseCommand):
 
     help = 'Runs the query observers'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--noreload', action='store_false', dest='use_reloader', default=True,
+                            help='Tells query observers to NOT use the auto-reloader.')
+
     def handle(self, *args, **options):
+        """
+        Runs the server, using the autoreloader if needed
+        """
+        use_reloader = options.get('use_reloader')
+
+        if use_reloader:
+            autoreload.main(self.inner_run, None, options)
+        else:
+            self.inner_run(None, **options)
+
+    def inner_run(self, *args, **options):
         # Check if we are using the correct database engine configuration.
         if db.connection.settings_dict['ENGINE'] != 'django_db_geventpool.backends.postgresql_psycopg2':
             raise exceptions.ImproperlyConfigured("Django REST Framework Reactive requires the geventpool database engine.")
