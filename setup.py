00ec697e62f66a81752c4939af49a800d7b2ab90
Tadej Janež <tadej.j@nez.si>
2018-10-22 15:07:41 +0200

    Overhaul and modernize setup.py

--- setup.py
+++ setup.py
@@ -1,60 +1,36 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import os.path
+import setuptools
 
-"""
-Making Django REST Framework reactive.
+# Get long description from README.
+with open('README.rst', 'r') as fh:
+    long_description = fh.read()
 
-See:
-https://github.com/genialis/django-rest-framework-reactive
-"""
-
-from setuptools import find_packages, setup
-# Use codecs' open for a consistent encoding
-from codecs import open
-from os import path
-
-base_dir = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(base_dir, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-
-# Get package metadata from 'rest_framework_reactive.__about__.py' file
+# Get package metadata from '__about__.py' file.
 about = {}
-with open(path.join(base_dir, 'rest_framework_reactive', '__about__.py'), encoding='utf-8') as f:
-    exec(f.read(), about)
+base_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(base_dir, 'rest_framework_reactive', '__about__.py'), 'r') as fh:
+    exec(fh.read(), about)
 
-setup(
+setuptools.setup(
     name=about['__title__'],
-
     version=about['__version__'],
-
     description=about['__summary__'],
     long_description=long_description,
-
-    url=about['__url__'],
-
+    long_description_content_type='text/x-rst',
     author=about['__author__'],
     author_email=about['__email__'],
-
+    url=about['__url__'],
     license=about['__license__'],
-
-    packages=find_packages(exclude=['tests']),
-    python_requires='>=3.6',
+    packages=setuptools.find_packages(exclude=['tests']),
+    python_requires='>=3.6, <3.7',
     install_requires=[
         'Django~=1.11.6',
         'djangorestframework>=3.4.0',
         'channels~=2.1.1',
     ],
     extras_require={
-        'docs': [
-            'sphinx>=1.3.2',
-            'sphinx_rtd_theme',
-        ],
-        'package': [
-            'twine',
-            'wheel',
-        ],
+        'docs': ['sphinx>=1.3.2', 'sphinx_rtd_theme'],
+        'package': ['twine', 'wheel'],
         'test': [
             'djangorestframework-filters~=0.10.0',
             # XXX: djangorestframework-filters has too open requirement for
@@ -62,11 +38,8 @@ setup(
             # have to pin it
             'django-filter~=1.0.0',
             'django-guardian>=1.4.2',
-            'django-jenkins>=0.17.0',
             'django-priority-batch>=1.0.0',
             'channels-redis~=2.1.0',
-            'coverage>=3.7.1',
-            'pep8>=1.6.2',
             'pylint>=1.4.3',
             'pytest~=3.5.1',
             'pytest-django~=3.2.1',
@@ -74,13 +47,10 @@ setup(
             'async_timeout>=2.0,<4.0',
             'psycopg2>=2.5.0',
             'check-manifest',
-            'readme',
         ],
     },
-
     classifiers=[
         'Development Status :: 4 - Beta',
-
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
@@ -89,15 +59,11 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: WSGI',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Libraries :: Python Modules',
-
         'License :: OSI Approved :: Apache Software License',
-
         'Operating System :: OS Independent',
-
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
     ],
-
     keywords='django-rest-framework reactive django',
 )
